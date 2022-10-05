42180e39989137b44743773a38bca298dda4c2b4
MOWLID OMAR <107882442+MowlidOmar@users.noreply.github.com>
2022-10-04 21:55:20 -0500

    Update duelDuo.test.ts

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -12,14 +12,28 @@ beforeEach(async () => {
 afterAll(async () => {
     driver.quit()
 })
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  expect(displayed).toBe(true);
+});
 
-test('div with id play-duo appears when bot is added', async () =>{
-    await driver.findElement(By.id('draw')).click();
-    await driver.sleep(2000)
-    await driver.findElement(By.css('bot-btn')).click();
-    await driver.sleep(2000)
-    const duoDiv = await driver.findElement(By.id('player-duo'));
+test("add to duo button should work giving div id", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver.sleep(2000);
+  await driver.findElement(By.css(".bot-btn")).click();
+  await driver.sleep(2000);
+  const duoDiv = await driver.findElement(By.id("player-duo"));
+  expect(duoDiv.isDisplayed()).toBeTruthy();
+});
 
-    expect(duoDiv.isDisplayed()).toBeTruthy();
+test("clicking the draw button displays the div with id of choices", async () => {
+  const draw = await driver.findElement(By.id("draw"));
+  draw.click();
+  await driver.sleep(2000);
+  const choices = await driver.findElement(By.id("choices"));
+  const displayed = await choices.isDisplayed();
 
-})
+  expect(displayed).toBe(true);
+});
+
